ce00e20d80a38774a7c751493eb6a24f9212827b
Roberto Li <1402927+robertoea@users.noreply.github.com>
2024-07-24 03:57:04 +0700

    Add iCloudBundleIdentifier and ubiquityKvstoreIdentifierSuffix

--- src/expo-plugin/types/index.ts
+++ src/expo-plugin/types/index.ts
@@ -3,4 +3,6 @@ export interface RNCloudStorageConfigPluginOptions {
    * The iCloud container environment to use. Defaults to 'Production'.
    */
   iCloudContainerEnvironment?: 'Production' | 'Development';
+  iCloudBundleIdentifier?: string;
+  ubiquityKvstoreIdentifierSuffix?: string;
 }
